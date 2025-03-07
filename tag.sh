1b8dca839ca52e332df87a0d1dcbb928413591ef
Omar Carey <omarcarey@Omars-MacBook-Pro.local>
2025-03-07 15:34:01 -0800

    update tag.sh file

--- tag.sh
+++ tag.sh
@@ -1,4 +1,5 @@
 TAG=$(echo $1)
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
 TAG_NAME=$(echo $TAG | cut -d'_' -f1)
 TAG_SEMVER=$(echo $TAG | cut -d'_' -f2)
@@ -12,3 +13,22 @@ PATCH_TAG=$(echo "${TAG_NAME}_${TAG_PARTS[0]}.${TAG_PARTS[1]}.${TAG_PARTS[2]}")
 echo Major Version Tag: $MAJOR_TAG
 echo Minor Version Tag: $MINOR_TAG
 echo Patch Version Tag: $PATCH_TAG
+
+CHECK=$(git tag | grep $PATCH_TAG)
+
+if [ $? == 0 ]; then
+    echo $PATCH_TAG tag alrady exists!
+    exit 1 
+fi
+
+echo "Tagging commit: $(git rev-parse HEAD)"
+git tag $MAJOR_TAG
+git tag $MINOR_TAG
+git tag $PATCH_TAG
+
+git push origin :refs/tags/$MAJOR_TAG
+git push origin :refs/tags/$MINOR_TAG
+
+git push origin $BRANCH_NAME tag $MAJOR_TAG
+git push origin $BRANCH_NAME tag $MINOR_TAG
+git push origin $BRANCH_NAME tag $PATCH_TAG
