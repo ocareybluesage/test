78301fcad1d14d0685308106a3f8ca57ba3d83f1
Omar Carey <omarcarey@Omars-MacBook-Pro.local>
2025-03-10 08:32:12 -0700

    add on pull request workflows

--- tag.sh
+++ tag.sh
@@ -1,4 +1,11 @@
 TAG=$(echo $1)
+
+CHECK=$(git tag | grep $TAG)
+if [ $? == 0 ]; then
+    echo $TAG tag alrady exists!
+    exit 1 
+fi
+
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 
 TAG_NAME=$(echo $TAG | cut -d'_' -f1)
@@ -14,24 +21,20 @@ echo Major Version Tag: $MAJOR_TAG
 echo Minor Version Tag: $MINOR_TAG
 echo Patch Version Tag: $PATCH_TAG
 
-CHECK=$(git tag | grep $PATCH_TAG)
-
-if [ $? == 0 ]; then
-    echo $PATCH_TAG tag alrady exists!
-    exit 1 
-fi
-
 echo "Tagging commit: $(git rev-parse HEAD)"
 git tag $MAJOR_TAG
 git tag $MINOR_TAG
 git tag $PATCH_TAG
 
+# Delete remote major/minor tags so that new major/minor tags can be created
 git push origin :refs/tags/$MAJOR_TAG
 git push origin :refs/tags/$MINOR_TAG
 
+# Push tags
 git push origin $BRANCH_NAME tag $MAJOR_TAG
 git push origin $BRANCH_NAME tag $MINOR_TAG
 git push origin $BRANCH_NAME tag $PATCH_TAG
 
+# Deletes local tag so future executions of this command run
 git tag --delete $MAJOR_TAG
 git tag --delete $MINOR_TAG
