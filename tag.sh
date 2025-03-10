55ddac5cdabe1b00d9d0984fc97570dca82bd298
Omar Carey <omarcarey@Omars-MacBook-Pro.local>
2025-03-10 11:18:41 -0700

    update tag script

--- tag.sh
+++ tag.sh
@@ -13,28 +13,22 @@ TAG_SEMVER=$(echo $TAG | cut -d'_' -f2)
 
 IFS='.' read -a TAG_PARTS <<< "$TAG_SEMVER"
 
-MAJOR_TAG=$(echo "${TAG_NAME}_${TAG_PARTS[0]}")
 MINOR_TAG=$(echo "${TAG_NAME}_${TAG_PARTS[0]}.${TAG_PARTS[1]}")
 PATCH_TAG=$(echo "${TAG_NAME}_${TAG_PARTS[0]}.${TAG_PARTS[1]}.${TAG_PARTS[2]}")
 
-echo Major Version Tag: $MAJOR_TAG
 echo Minor Version Tag: $MINOR_TAG
 echo Patch Version Tag: $PATCH_TAG
 
 echo "Tagging commit: $(git rev-parse HEAD)"
-git tag $MAJOR_TAG
 git tag $MINOR_TAG
 git tag $PATCH_TAG
 
 # Delete remote major/minor tags so that new major/minor tags can be created
-git push origin :refs/tags/$MAJOR_TAG
 git push origin :refs/tags/$MINOR_TAG
 
 # Push tags
-git push origin $BRANCH_NAME tag $MAJOR_TAG
 git push origin $BRANCH_NAME tag $MINOR_TAG
 git push origin $BRANCH_NAME tag $PATCH_TAG
 
 # Deletes local tag so future executions of this command run
-git tag --delete $MAJOR_TAG
 git tag --delete $MINOR_TAG
